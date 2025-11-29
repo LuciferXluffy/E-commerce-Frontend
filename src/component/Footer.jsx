de5e93f54c4ba1b41ce124023e50c36d903b01d6
sahilsamaiya <sahillihas050@gmail.com>
2025-11-30 03:16:29 +0530

    ..

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white font-grotesk px-10 py-10 absolute bottom-0 w-full">
+    <footer className="bg-[url('/Batman.jpg')] w-full bg-cover bg-center bg-no-repeat py-10 px-10 text-white">
       <div className="grid grid-cols-3 gap-2 text-center">
         {/* Get in Touch Coloumn */}
         <div className="flex flex-col gap-5 ml-10">
@@ -45,26 +45,76 @@ const Footer = () => {
         </div>
 
         {/* Social Column */}
-        <div className="text-[20px]">SOCIAL</div>
+        <div className="flex flex-col gap-5 ml-30">
+          <div className="flex items-center gap-2">
+            <img src="/socialLogo.png" alt="Social Logo" className="w-6 h-6" />
+            <div className="text-[20px]">SOCIAL</div>
+          </div>
+          <div className="flex items-center gap-5 ml-5 mt-5">
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/instagram.png"
+                alt="Instagram Logo"
+                className="w-8 h-8"
+              />
+            </a>
+
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/facebook.png"
+                alt="Facebook Logo"
+                className="w-8 h-8"
+              />
+            </a>
+          </div>
+          <div className="w-80 border-b-2 border-gray-400 mt-5"></div>
+          <div className="flex flex-col gap-5 text-left">
+            <div class="flex items-center gap-2">
+              <img
+                src="/About Us.png"
+                alt="About UsLogo"
+                className="w-12 h-10"
+              />
+              <div className="text-[20px]">ABOUT US</div>
+            </div>
+            <div className="ml-10 flex flex-col gap-2">
+              <div className="text-[14px] hover:text-yellow-500">ABOUT US</div>
+              <div className="text-[14px] hover:text-yellow-500">
+                THE BEGINNING
+              </div>
+              <div className="text-[14px] hover:text-yellow-500">
+                THE INFINITY
+              </div>
+            </div>
+          </div>
+        </div>
 
         {/* Quick Links Coloumn */}
         <div className="flex flex-col gap-2 text-left ml-40">
           <div className="flex items-center gap-2 mb-5">
-            <img
-              src="/link.png"
-              alt="quick link Logo"
-              className="w-5 h-4"
-            />
+            <img src="/link.png" alt="quick link Logo" className="w-5 h-4" />
             <div className="text-[20px]">QUICK LINK</div>
           </div>
           <div className="text-[14px] hover:text-yellow-500">HOME</div>
-          <div className="text-[14px] hover:text-yellow-500" >MY ACCOUNT</div>
+          <div className="text-[14px] hover:text-yellow-500">MY ACCOUNT</div>
           <div className="text-[14px] hover:text-yellow-500">CONTACT US</div>
           <div className="text-[14px] hover:text-yellow-500">FAQ</div>
           <div className="text-[14px] hover:text-yellow-500">T&C</div>
-          <div className="text-[14px] hover:text-yellow-500">PRIVACY POLICY</div>
+          <div className="text-[14px] hover:text-yellow-500">
+            PRIVACY POLICY
+          </div>
           <div className="text-[14px] hover:text-yellow-500">REFUND POLICY</div>
-          <div className="text-[14px] hover:text-yellow-500">RETURN AND EXCHANGE POLICY</div>
+          <div className="text-[14px] hover:text-yellow-500">
+            RETURN AND EXCHANGE POLICY
+          </div>
         </div>
       </div>
     </footer>
